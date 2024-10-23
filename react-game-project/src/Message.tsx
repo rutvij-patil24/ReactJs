4bc0453eea3f6249201908d592c4a67d8f7d7e91
Rutvij Patil <66994155+rutvij-patil24@users.noreply.github.com>
2024-10-23 19:01:39 -0400

    Added Bootstrap

---
+++ react-game-project/src/Message.tsx
@@ -0,0 +1,10 @@
+
+
+const Message = () => {
+    const name = 'Rutvij';
+    if(name == 'Rutvij')
+        return <h1>Hello {name}!</h1>;
+    return <h1>Hello World!</h1>;
+};
+
+export default Message;
