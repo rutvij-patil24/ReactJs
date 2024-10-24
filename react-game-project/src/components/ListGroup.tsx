8c966f93295b7634d6fc848034f4187388e127a1
Rutvij Patil <66994155+rutvij-patil24@users.noreply.github.com>
2024-10-23 20:11:12 -0400

    Performed event management and hooks

--- react-game-project/src/components/ListGroup.tsx
+++ react-game-project/src/components/ListGroup.tsx
@@ -1,20 +1,32 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+// import { MouseEvent } from "react";
 
-let items = ["Mumbai", "Kolkata", "New York", "Tokyo", "Los Angeles"];
-// items = [];
 
-const emptyList = () => {
-  return items.length === 0 && <p>No item found!</p>;
-};
+// const emptyList = () => {
+//   return items.length === 0 && <p>No item found!</p>;
+// };
+
+// Event Handler
+// const handleClick = (event: MouseEvent) => alert("Clicked " + event.clientX);
 
 const ListGroup = () => {
+    let items = ["Mumbai", "Kolkata", "New York", "Tokyo", "Los Angeles"];
+    // Hook
+    const[selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <Fragment>
       <h1>List</h1>
-      {emptyList()}
+      {/* {emptyList()} */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
